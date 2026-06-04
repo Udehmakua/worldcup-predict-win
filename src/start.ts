c12e8c10d861c7a1cde9ad9244415f14c658673d
Udehmakua <94395143+Udehmakua@users.noreply.github.com>
2026-06-04 20:44:05 +0100

    Update start.ts

--- src/start.ts
+++ src/start.ts
@@ -17,7 +17,7 @@ const errorMiddleware = createMiddleware().server(async ({ next }) => {
     });
   }
 });
-
+//
 export const startInstance = createStart(() => ({
   functionMiddleware: [attachSupabaseAuth],
   requestMiddleware: [errorMiddleware],
